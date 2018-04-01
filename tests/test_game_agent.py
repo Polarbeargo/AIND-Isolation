4f12910696dcae3cfc78c961fad1703a6b5e7086
alice <bow1226@gmail.com>
2018-04-01 08:46:30 +0800

    Add unittest
    Add unittest

--- tests/test_game_agent.py
+++ tests/test_game_agent.py
@@ -16,14 +16,14 @@ class IsolationTest(unittest.TestCase):
 
     def setUp(self):
         reload(game_agent)
-        self.player1 = "Player1"
-        self.player2 = "Player2"
+        self.player1 = game_agent.MinimaxPlayer()
+        self.player2 = game_agent.MinimaxPlayer()
         self.game = isolation.Board(self.player1, self.player2)
 
-    def test_example(self):
-        # TODO: All methods must start with "test_"
-        self.fail("Hello, World!")
-
+    def play(self):
+        print("starting game")
+        self.game.play()
+        self.assertEqual(True, True)
 
 if __name__ == '__main__':
     unittest.main()
